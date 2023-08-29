1c844710c9774a15b10c0b7850d804adea9a1fa5
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-08-29 17:17:01 +0700

    new change

--- database.js
+++ database.js
@@ -1,9 +1,21 @@
 const mysql = require("mysql2");
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   port: "3306",
+//   password: "",
+//   database: "oas",
+// });
+
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   port: "3306",
-  password: "",
+  password: "@P@SS.W0rd",
   database: "oas",
 });
 
