b1731e1db8d3a8a89865698b0df472780a63a689
Siwakorn Banluesapy <siwakorn167@gmail.com>
2024-06-24 21:00:35 +0700

    New Change 24062024

--- common.js
+++ common.js
@@ -2,63 +2,68 @@ const d = {};
 d.drivinglicense_type = [
   {
     dlt_code: "A",
-    dlt_description: "A : Two-wheels motocycle engine not exceed 125 cc",
+    dlt_name_lo: "ລົດຈັກສອງລໍ້ ຄວາມແຮງບໍ່ເກີນ 125 cc",
+    dlt_name_eng: "Two-wheels motocycle engine not exceed 125 cc",
   },
   {
     dlt_code: "A1",
-    dlt_description: "A1 : Two-wheels motorcycle engine 125 cc and above",
+    dlt_name_lo: "ລົດຈັກສອງລໍ້ ຄວາມແຮງ ແຕ່ 125 cc ຂື້ນໄປ",
+    dlt_name_eng: "Two-wheels motorcycle engine 125 cc and above",
   },
   {
     dlt_code: "A2",
-    dlt_description:
-      "A2 : Private three-wheels vehicle, Passenger vehicle with two-wheels and three-wheels",
+    dlt_name_lo: "ລົດຈັກສາມລໍ້ສ່ວນຕົວ, ລົດສອງລໍ້ ແລະ ສາມລໍ້ໂດຍສານ",
+    dlt_name_eng: "Private three-wheels vehicle, Passenger vehicle with two-wheels and three-wheels",
   },
   {
     dlt_code: "A3",
-    dlt_description:
-      "A3 : Two-wheels tractor with trailer and Walk-behide Tractor",
+    dlt_name_lo: "ລົດໄຖນາທີ່ແກ່ລໍ້ ແລະ ລົດຕ໋ອກໆ",
+    dlt_name_eng: "Two-wheels tractor with trailer and Walk-behide Tractor",
   },
   {
     dlt_code: "B",
-    dlt_description:
-      "B : Car with four-wheels, total weight not exceed 3,500 kilograms, and not more than 9 seats including driver",
+    dlt_name_lo: "ລົດເບົາ ສີ່ລໍ້ ທີ່ມີນ້ຳໜັກລວມບໍ່ເກີນ 3.500 ກິໂລກຣາມ ແລະ ບໍ່ເກີນ 9 ບ່ອນນັ່ງ ລວມທັງຜູ້ຂັບຂີ່",
+    dlt_name_eng: "Car with four-wheels, total weight not exceed 3,500 kilograms, and not more than 9 seats including driver",
   },
   {
     dlt_code: "C",
-    dlt_description:
-      "C : Cargo truck, total weight from 3,500 to 7,500 kilograms",
+    dlt_name_lo: "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ແຕ່ 3.500 ກິໂລກຣາມ ຫາ 7.500 ກິໂລກຣາມ",
+    dlt_name_eng: "Cargo truck, total weight from 3,500 to 7,500 kilograms",
   },
   {
     dlt_code: "C1",
-    dlt_description:
-      "C1 : Cargo truck, total weight exceed 7,500 kilogram up to 15,000 kilograms",
+    dlt_name_lo: "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ເກີນກວ່າ 7.500ກິໂລກຣາມ ຫາ 15.000 ກິໂລກຣາມ",
+    dlt_name_eng: "Cargo truck, total weight exceed 7,500 kilogram up to 15,000 kilograms",
   },
   {
     dlt_code: "C2",
-    dlt_description: "C2 : Cargo truck, total weight exceed 15,000 kilograms",
+    dlt_name_lo: "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ເກີນກວ່າ 15.000 ກິໂລກຣາມ",
+    dlt_name_eng: "Cargo truck, total weight exceed 15,000 kilograms",
   },
   {
     dlt_code: "D",
-    dlt_description:
-      "D : Passenger vehicle with 4 wheels or more, not more than 15 seats",
+    dlt_name_lo: "ລົດຂົນສົ່ງຜູ້ໂດຍສານປະເພດ 4 ລໍ້ ຂຶ້ນໄປ, ບໍ່ເກີນ ສິບຫ້າ ບ່ອນນັ່ງ",
+    dlt_name_eng: "Passenger vehicle with 4 wheels or more, not more than 15 seats",
   },
   {
     dlt_code: "D1",
-    dlt_description: "D1 : Passenger vehicle from 16 seats to 35 seats",
+    dlt_name_lo: "ລົດຂົນສົ່ງຜູ້ໂດຍສານ ແຕ່ ສິບຫົກ ຫາ ສາມສິບຫ້າ ບ່ອນນັ່ງ",
+    dlt_name_eng: "Passenger vehicle from 16 seats to 35 seats",
   },
   {
     dlt_code: "D2",
-    dlt_description: "D2 : Passenger vehicle from 36 seats or more",
+    dlt_name_lo: "ລົດຂົນສົ່ງຜູ້ໂດຍສານ ແຕ່ ສາມສິບຫົກ ບ່ອນນັ່ງ ຂຶ້ນໄປ",
+    dlt_name_eng: "Passenger vehicle from 36 seats or more",
   },
   {
     dlt_code: "E",
-    dlt_description:
-      "E : Car (B), Cargo truck (C) and Passenger vehicle (D), having trailer total weight not exceed 750 kilograms",
+    dlt_name_lo: "ລົດເບົາ (B), ລົດຂົນສົ່ງສິນຄ້າ (C) ແລະ ລົດຂົນສົ່ງຜູ້ໂດຍສານ (D) ທີ່ແກ່ຫາງລາກນ້ຳໜັກລວມບໍ່ເກີນ 750 ກິໂລກຣາມ",
+    dlt_name_eng: "Car (B), Cargo truck (C) and Passenger vehicle (D), having trailer total weight not exceed 750 kilograms",
   },
   {
     dlt_code: "E1",
-    dlt_description:
-      "E1 : Cargo truck (C2) having trailer total weight exceed 750 kilograms",
+    dlt_name_lo: "ລົດຂົນສົ່ງສິນຄ້າ (C2) ແກ່ຫາງລາກນ້ຳໜັກລວມເກີນກວ່າ 750 ກິໂລກຣາມ",
+    dlt_name_eng: "Cargo truck (C2) having trailer total weight exceed 750 kilograms",
   },
 ];
 d.sms_token =
