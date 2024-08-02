aaf40210553d78b05deb30ab2b7327cac575d050
Siwakorn Banluesapy <siwakorn167@gmail.com>
2024-08-02 15:31:07 +0700

    Update 02082024

--- routers/report.js
+++ routers/report.js
@@ -44,7 +44,7 @@ router.post("/course/:course_id", middleware, async (req, res, next) => {
   let sql_count =
   " SELECT  COUNT(*) as numRows FROM  app_course_log t1 INNER JOIN app_user t2 ON  t2.user_id = t1.user_id INNER JOIN app_course t3 ON  t3.course_id = t1.course_id  WHERE  t1.course_id = ? ";
 
-  const getCountAll = await runQuery(sql_count, p.concat(search_param));
+  const getCountAll = await runQuery(sql_count + " GROUP BY t1.user_id ", p.concat(search_param));
   const total = getCountAll[0] !== undefined ? getCountAll[0]?.numRows : 0;
 
   if (search !== "" || search.length > 0) {
@@ -63,7 +63,7 @@ router.post("/course/:course_id", middleware, async (req, res, next) => {
     ];
   }
   sql += ` GROUP BY t1.user_id  ORDER BY t1.cl_id DESC LIMIT ${offset},${per_page} `;
-  const getCountFilter = await runQuery(sql_count, p.concat(search_param));
+  const getCountFilter = await runQuery(sql_count + " GROUP BY t1.user_id ", p.concat(search_param));
   const total_filter =
     getCountFilter[0] !== undefined ? getCountFilter[0]?.numRows : 0;
 
